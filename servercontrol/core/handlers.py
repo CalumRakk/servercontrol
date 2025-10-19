ca1ea4718465da3ba13a4e393af98ad111a90701
Leo <leocasti2@gmail.com>
2025-10-19 02:56:57 -0500

    refactor: actualiza la forma de agregar comandos de telegram a register_handlers

--- servercontrol/core/handlers.py
+++ servercontrol/core/handlers.py
@@ -1,8 +1,10 @@
 from pyrogram import Client, filters  # type: ignore
+from pyrogram.handlers import MessageHandler  # type: ignore
+
+from .commands import echo
 
 
-def register_handlers(app: Client):
-    @app.on_message(filters.text & filters.private)
-    def echo(client, message):
-        message.reply(message.text)
+def register_handlers(app: Client): 
+
+    app.add_handler(MessageHandler(echo, filters.text & filters.private))
         
