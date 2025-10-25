2728c14b64a5bdd5b8ad28a523f98348cfc921f7
Leo <leocasti2@gmail.com>
2025-10-25 00:56:58 -0500

    feat: implementa comandos para iniciar y detener AWS, actualiza el manejo de estados

--- servercontrol/core/commands.py
+++ servercontrol/core/commands.py
@@ -1,8 +1,7 @@
 
 
 from labcontrol.config import Config as LabConfig
-from labcontrol.parser import parse_lab_aws_details_content
-from labcontrol.schema import AWSContent, AWSStatus
+from labcontrol.schema import AWSStatus, LabStatus
 from labcontrol.utils import get_params_with_config
 from labcontrol.vocareum_http import VocareumApi
 from pyrogram import enums
@@ -33,7 +32,33 @@ def get_aws_status(client, message, config: LabConfig):
     except Exception as e:
         message.reply(str(e))
 
-def get_aws(client, message, config: LabConfig):
+# def get_aws(client, message, config: LabConfig):
+#     try:
+#         if should_wait_get_params(config):
+#             message.reply("Esperando a que se obtenga la información de Vocareum. Esto puede toma un tiempo.")
+
+#         params= get_params_with_config(config)
+#         vocareum_api = VocareumApi(params)
+
+#         result = vocareum_api.get_aws()
+#         if isinstance(result, AWSStatus):
+#             if result.success is True:
+#                 msg= f"Parece que la maquina se esta encendiendo. Por favor, espera un momento. Lab status: {result.status.value}"
+#                 message.reply(msg)
+#                 return
+#         elif isinstance(result, AWSContent):
+#             if result.success is True:
+#                 content_parsed= parse_lab_aws_details_content(result.content)
+#                 message.reply(content_parsed.model_dump_json(indent=4), parse_mode=enums.ParseMode.MARKDOWN)
+#                 return
+            
+#         message.reply("No se pudo obtener el estado de AWS. Intente de nuevo.")
+#         config.vocareum_cookies_path.unlink(missing_ok=True)
+#     except Exception as e:
+#         message.reply(str(e))
+
+
+def start_aws(client, message, config: LabConfig):
     try:
         if should_wait_get_params(config):
             message.reply("Esperando a que se obtenga la información de Vocareum. Esto puede toma un tiempo.")
@@ -41,18 +66,56 @@ def get_aws(client, message, config: LabConfig):
         params= get_params_with_config(config)
         vocareum_api = VocareumApi(params)
 
-        result = vocareum_api.get_aws()
+        stus_result= vocareum_api.get_aws_status()
+        if stus_result.success is True:
+            if stus_result.status == LabStatus.in_creation:
+                message.reply("Encendiendo una maquina que se esta encendiendo...")
+                return
+            elif stus_result.status == LabStatus.ready:  
+                message.reply("Encendiendo una maquina encendida...")         
+
+        result = vocareum_api.start_aws()
         if isinstance(result, AWSStatus):
             if result.success is True:
                 msg= f"Parece que la maquina se esta encendiendo. Por favor, espera un momento. Lab status: {result.status.value}"
                 message.reply(msg)
                 return
-        elif isinstance(result, AWSContent):
-            if result.success is True:
-                content_parsed= parse_lab_aws_details_content(result.content)
-                message.reply(content_parsed.model_dump_json(indent=4), parse_mode=enums.ParseMode.MARKDOWN)
-                return
+  
+        if result.success is True:            
+            message.reply("Maquina en creación.", parse_mode=enums.ParseMode.MARKDOWN)
+            return
             
+        message.reply("No se pudo obtener el estado de AWS. Intente de nuevo.")
+        config.vocareum_cookies_path.unlink(missing_ok=True)
+    except Exception as e:
+        message.reply(str(e))
+
+def end_aws(client, message, config: LabConfig):
+    try:
+        if should_wait_get_params(config):
+            message.reply("Esperando a que se obtenga la información de Vocareum. Esto puede toma un tiempo.")
+
+        params= get_params_with_config(config)
+        vocareum_api = VocareumApi(params)
+
+        stus_result= vocareum_api.get_aws_status()
+        if stus_result.success is True:
+            if stus_result.status == LabStatus.in_creation:
+                message.reply("Apagando una maquina que se está encendiendo...")    
+            elif stus_result.status == LabStatus.ready:  
+                message.reply("Apagando una maquina encendida...")         
+
+        result = vocareum_api.end_aws()
+        if isinstance(result, AWSStatus):
+            if result.success is True:
+                msg= f"Parece que la maquina se esta encendiendo o apagando.. Por favor, espera un momento. Lab status: {result.status.value}"
+                message.reply(msg)
+                return
+   
+        if result.success is True:
+            message.reply("Maquina detenida.", parse_mode=enums.ParseMode.MARKDOWN)
+            return
+        
         message.reply("No se pudo obtener el estado de AWS. Intente de nuevo.")
         config.vocareum_cookies_path.unlink(missing_ok=True)
     except Exception as e:
--- servercontrol/core/handlers.py
+++ servercontrol/core/handlers.py
@@ -2,12 +2,14 @@ from labcontrol.config import Config as LabConfig
 from pyrogram import Client, filters  # type: ignore
 from pyrogram.handlers import MessageHandler  # type: ignore
 
-from .commands import get_aws, get_aws_status, help
+from .commands import end_aws, get_aws_status, help, start_aws
 
 
 def register_handlers(app: Client, config: LabConfig): 
 
     # app.add_handler(MessageHandler(echo, filters.text & filters.private))
     app.add_handler(MessageHandler(lambda client, message: get_aws_status(client, message, config), filters.command("status") & filters.private))
-    app.add_handler(MessageHandler(lambda client, message: get_aws(client, message, config), filters.command("content") & filters.private))
+    app.add_handler(MessageHandler(lambda client, message: start_aws(client, message, config), filters.command("start") & filters.private))
+    app.add_handler(MessageHandler(lambda client, message: end_aws(client, message, config), filters.command("end") & filters.private))
+    # app.add_handler(MessageHandler(lambda client, message: get_aws(client, message, config), filters.command("content") & filters.private))
     app.add_handler(MessageHandler(help, filters.command("help") & filters.private))
